0f5e30ee917ff74a070bc421993a59feef331795
Nekdil01 <Nekdil01@mail.ru>
2020-03-16 10:10:12 +0300

    Homework 3: added "range" methods

--- src/com/sbt/jscool/homework3/Homework3.java
+++ src/com/sbt/jscool/homework3/Homework3.java
@@ -2,9 +2,23 @@ package com.sbt.jscool.homework3;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Homework3 {
+
+    public static class MyComp implements Comparator<Integer>{
+
+        @Override
+        public int compare(Integer integer, Integer t1) {
+            return integer.compareTo(t1);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return o.equals(this);
+        }
+    }
 
     public static void main(String[] args) {
         /*CountMap<Integer> countMap = new CountMapInt<>();
@@ -18,18 +32,19 @@ public class Homework3 {
         System.out.println(countMap.getCount(6)); // 1
         System.out.println(countMap.getCount(10)); // 3*/
 
-        List<Number> numberList = new ArrayList<>();
+        List<Integer> numberList = new ArrayList<>();
         numberList.add(1);
         numberList.add(2);
         numberList.add(3);
         numberList.add(4);
+        numberList.add(4);
+        numberList.add(5);
+        numberList.add(6);
+        numberList.add(7);
+        Integer min = 3;
+        Integer max = 6;
 
-        List<Integer> integerList = new ArrayList<>();
-        integerList.add(1);
-        integerList.add(4);
 
-        //CollectionUtils.removeAll(numberList,integerList);
-        System.out.println(CollectionUtils.containsAny(numberList,integerList));
-
+        System.out.println(CollectionUtils.range(numberList,min,max,new MyComp()));
     }
 }
